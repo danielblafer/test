7f3063497a4e30c709c025e37f0bc7b13ade124e
danielblafer <44142877+danielblafer@users.noreply.github.com>
2024-05-21 00:53:30 +0200

    Create ads_2.js

---
+++ ads_2.js
@@ -0,0 +1,7 @@
+function addStyle(e){const t=document.createElement("style");t.textContent=e,document.head.append(t)}
+
+addStyle(`.aaa{width: 500px; height: 500px; background: red; font-size: 2em;}`);
+        
+
+//-----------
+$( ".publicidad" ).html("<div class='aaa'>bbbbbbbb</div>");
