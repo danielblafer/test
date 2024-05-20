bce9ecfde9acf875fdf1171d12f27b8ad6efb9c0
danielblafer <44142877+danielblafer@users.noreply.github.com>
2024-05-21 00:56:24 +0200

    Create ads_3.js

---
+++ ads_3.js
@@ -0,0 +1,7 @@
+function addStyle(e){const t=document.createElement("style");t.textContent=e,document.head.append(t)}
+
+addStyle(`.aaa{width: 500px; height: 500px; background: blue; font-size: 2em;}`);
+        
+
+//-----------
+$( ".publicidad" ).html("<div class='aaa'>ccccccc</div>");
